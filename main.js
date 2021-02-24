f81f1e98260ee7db6808fb523ca4c75c47e6e11e
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-02-24 17:56:42 +0700

    [ADD] Gallery5,preview

--- main.js
+++ main.js
@@ -4,20 +4,14 @@ const app = Vue.createApp({
             gallerys:[{img:"images/Gallery1.jpg",address:"Kyoto,Japan",favorite:false,show:true},
                      {img:"images/Gallery2.jpg",address:"Tokyo,Japan",favorite:false,show:true},
                      {img:"images/Gallery3.jpg",address:"Wonderland,Kiki",favorite:false,show:true},
-                     {img:"images/Gallery4.jpg",address:"Flying pig,Italy",favorite:false,show:true}],
-            imgShow:0,
+                     {img:"images/Gallery4.jpg",address:"Flying pig,Italy",favorite:false,show:true},
+                     {img:"images/Gallery5.jpg",address:"Ponyo Kyushu,Japan",favorite:false,show:true}],
             focusImg:{show:false,index:0},
         }
     },
     methods: {
         favoriteImg(index){
             this.gallerys[index].favorite = !this.gallerys[index].favorite
-        },
-        changfocusImg(index){
-            if(!this.focusImg.show){
-                this.focusImg.show = true
-            }
-            this.focusImg.index = index
         },
     },
     computed:{
